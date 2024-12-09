c60809aaceed6b44751f4f35ee6497f92178ae56
alidaakata <akataalida@gmail.com>
2024-12-09 17:16:17 +0100

    asm03-revue

--- asm03/asm03.s
+++ asm03/asm03.s
@@ -6,29 +6,36 @@ section .text
 global _start
 
 _start:
-    ; Récupérer l'adresse du premier paramètre
-    mov rsi, [rsp + 16] ; argv[1]
-    cmp byte [rsi], '4'
-    jne error
-    cmp byte [rsi + 1], '2'
-    jne error
-    cmp byte [rsi + 2], 0
-    jne error
+    ; Vérifier que nous avons au moins un argument
+    mov rdi, [rsp + 8]    ; rdi pointe sur argc
+    cmp rdi, 1             ; On vérifie si argc > 1
+    jle error              ; Si argc <= 1, on va directement à l'erreur
+
+    ; Récupérer l'adresse du premier paramètre (argv[1])
+    mov rsi, [rsp + 16]    ; rsi pointe sur argv[1]
+
+    ; Comparaison avec "42"
+    cmp byte [rsi], '4'    ; Vérifier si le premier caractère est '4'
+    jne error              ; Si ce n'est pas '4', aller à l'erreur
+    cmp byte [rsi + 1], '2' ; Vérifier si le deuxième caractère est '2'
+    jne error              ; Si ce n'est pas '2', aller à l'erreur
+    cmp byte [rsi + 2], 0   ; Vérifier la fin de la chaîne (doit être '\0')
+    jne error              ; Si ce n'est pas la fin de la chaîne, aller à l'erreur
 
     ; Affichage "1337"
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, correct
-    mov rdx, len_correct
+    mov rax, 1             ; syscall: write
+    mov rdi, 1             ; stdout
+    mov rsi, correct       ; buffer contenant "1337\n"
+    mov rdx, len_correct   ; taille de la chaîne "1337\n"
     syscall
 
-    ; Retourne 0
-    mov rax, 60
-    xor rdi, rdi
+    ; Retourne 0 (indique succès)
+    mov rax, 60            ; syscall: exit
+    xor rdi, rdi           ; code de sortie 0
     syscall
 
 error:
-    ; Retourne 1
-    mov rax, 60
-    mov rdi, 1
+    ; Retourne 1 (indique échec)
+    mov rax, 60            ; syscall: exit
+    mov rdi, 1             ; code de sortie 1
     syscall
